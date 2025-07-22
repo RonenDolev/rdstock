d31ec28549e728b8003f15d78413df410bf06bfe
RonenDolev <ronen.dolev@gmail.com>
2025-07-22 12:42:36 +0300

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.ronen.dstock',
-  appName: 'dstock',
-  webDir: 'public'
-};
-
-export default config;
